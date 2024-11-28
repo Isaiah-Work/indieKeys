15cd49255d67bc8ffa4708f0495e22366ba5c2ea
Isaiah-Work <isaiah.rivasc@gmail.com>
2024-11-27 22:17:46 -0600

    .

--- public/paneladmin.php
+++ public/paneladmin.php
@@ -7,7 +7,7 @@
 
 <div class="container-fluid mt-5">
         <div class=" p-5 text-center ">
-            <h1 class="fw-bold mb-4" ">Panel de Administración</h1>
+            <h1 class="fw-bold mb-4" >Panel de Administración</h1>
             <p class="mb-5">
                 Bienvenido al lugar donde puedes gestionar todos los productos de IndieKeys. Realiza operaciones de creación, edición y eliminación de productos de forma rápida y sencilla.
             </p>
